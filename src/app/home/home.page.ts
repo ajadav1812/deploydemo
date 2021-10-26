1daa7a8d5fae037bb8160a633455438dbb227f70
AakashJadav <aakashjadav.rao@gmail.com>
2021-10-26 12:55:16 +0530

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { Plugins } from '@capacitor/core';
+import 'contacts-plugin';
+const {ContactsPlugin} = Plugins 
+const {message} = Plugins
+import 'message'
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +14,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  contacts = [];
 
+  constructor(private sqlite: SQLite) {}
+
+  async btnclick(){
+
+    // this.contacts = (await message.getMessage('hi')).results;
+    // alert('data.. '+ JSON.stringify(this.contacts));
+
+
+    this.contacts = (await ContactsPlugin.getContacts('hi')).results;
+    alert('my contacts .. '+ JSON.stringify(this.contacts));
+    //alert('welcome..')
+
+  //   this.sqlite.create({
+  //     name: 'data.db',
+  //     location: 'default'
+  //   })
+  //     .then((db: SQLiteObject) => {
+    
+    
+  //       db.executeSql('create table danceMoves(name VARCHAR(32))', [])
+  //         .then(() => alert('Executed SQL'))
+  //         .catch(e => console.log(e));
+    
+    
+  //     })
+  //     .catch(e => console.log(e));
+   }
+
+
+   btnHotCodePush(){
+      alert();
+   }
 }
